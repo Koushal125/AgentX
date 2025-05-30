446ea29ca750ba68cb892121b1329678d61b0d4e
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-05-30 11:35:12 +0530

    minor correctoins

--- server/main.py
+++ server/main.py
@@ -13,10 +13,12 @@ class PromptInput(BaseModel):
 def parse_prompt(input: PromptInput):
     try:
 
+        instruction = "consider the user prompt and return a json consisting of "
+
         model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
 
         chat = model.start_chat()
-        response = chat.send_message(input.prompt)
+        response = chat.send_message(instruction + input.prompt)
 
         return {"response": response.text}
     except Exception as e:
